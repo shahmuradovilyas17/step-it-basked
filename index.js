6d66f9bee54201e022d3ba8c7e21b4a545eee8d4
Huseynov Turab Taryel <huseynov_t@itstep.edu.az>
2024-06-12 21:47:29 +0400

    development process basket

--- index.js
+++ index.js
@@ -42,6 +42,32 @@ const init = () => {
   console.log(productsArr);
   document.addEventListener("click", (e) => {
     const target = e.target;
+    if (target.closest(".product-item-basket-icon")) {
+      addToBasket(target);
+    }
+    if (target.closest(".header-basket-icon")) {
+      document.querySelector(".basket-inner").style.display = "flex";
+    }
+    if (target.closest(".basket-close")) {
+      document.querySelector(".basket-inner").style.display = "none";
+    }
+    if (target.closest(".product-plus")) {
+      plusProductCount(target);
+    }
+    if (target.closest(".product-minus")) {
+      minusProductCount(target);
+    }
+    if (target.closest(".delete-item")) {
+      deleteBasketItem(target);
+    }
+    document.addEventListener("keydown", (e) => {
+      if (e.keyCode === 27) {
+        document.querySelector(".basket-inner").style.display = "none";
+      }
+    });
+    if (target.closest(".basket-inner") && !target.closest(".basket-window")) {
+      document.querySelector(".basket-inner").style.display = "none";
+    }
   });
 
   function renderItems(productsArr) {
@@ -86,9 +112,74 @@ const init = () => {
   }
   loadItem();
 
+  function addToBasket(target) {
+    const productItem = target.closest(".product-item");
+    const itemId = productItem.dataset.id;
+    const productCount = parseInt(
+      productItem.querySelector(".product-count").textContent
+    );
+    const product = productsArr.find((item) => {
+      console.log("item.id", item.id);
+      console.log("itemId", itemId);
+      console.log("item.id === itemId", +item.id === +itemId);
+      return +item.id === +itemId;
+    });
 
+    console.log("product", product);
 
+    const basketItem = basketArr.find((item) => {
+      return +item.id === +itemId;
+    });
+    if (basketItem) {
+      basketItem.count += productCount;
+    } else {
+      const basketProduct = product;
+      basketProduct.count = productCount;
+      basketArr.push(basketProduct);
+    }
+    productItem.querySelector(".product-count").textContent = 1;
+    renderBasket();
+    totalPriceBlock();
+    totalBasketCount();
+    saveCartToLocalStorage();
 
+    console.log(basketArr);
+  }
+
+  function renderBasket() {
+    let basketDivItem = "";
+
+    basket.innerText = "";
+    basketArr.forEach((i) => {
+      basketDivItem += `
+      <div class="product-item" data-id="${i.id}">
+      <img
+        src="${i.thumbnail}"
+        alt=""
+      />
+      <div class="product-brand">${i.brand}</div>
+      <div class="product-name">
+      ${i.title}
+      </div>
+      <div class="product-desc">
+      ${i.description}
+      </div>
+      <div class="product-price">$${i.price}</div>
+      <div class="product-total-price">$${i.price * i.count}</div>
+      <div class="product-func">
+      <div class="product-num">
+        <div class="product-minus">-</div>
+        <div class="product-count">${i.count}</div>
+        <div class="product-plus">+</div>
+      </div>
+      <img class="delete-item" src="img/delete-svgrepo-com.svg" alt="" />
+        </div>
+      </div>
+    </div>
+      `;
+    });
+    basket.innerHTML = basketDivItem;
+  }
 
   function plusProductCount(target) {
     const productNumDiv = target.closest(".product-num");
@@ -100,7 +191,16 @@ const init = () => {
 
     const product = target.closest(".product-item");
     const productId = product.dataset.id;
-
+    const basketItem = basketArr.find((item) => {
+      return +item.id === +productId;
+    });
+    if (basketItem) {
+      basketItem.count = productNumber;
+      product.querySelector(".product-total-price").textContent = `${
+        basketItem.price * basketItem.count
+      }$`;
+    }
+    totalBasketCount();
     totalPriceBlock();
     saveCartToLocalStorage();
   }
@@ -110,22 +210,56 @@ const init = () => {
     const productCount = productNumDiv.querySelector(".product-count");
 
     let productNumber = +productCount.textContent;
-
+    if (productNumber === 1 && target.closest(".basket-items")) {
+      deleteBasketItem(target);
+    }
     if (productNumber > 1) {
       productNumber -= 1;
       productCount.textContent = productNumber;
 
       const product = target.closest(".product-item");
       const productId = product.dataset.id;
+      const basketItem = basketArr.find((item) => {
+        return +item.id === +productId;
+      });
+      if (basketItem) {
+        basketItem.count = productNumber;
+        product.querySelector(".product-total-price").textContent = `${
+          basketItem.price * basketItem.count
+        }$`;
+      }
     }
 
+    totalBasketCount();
     totalPriceBlock();
     saveCartToLocalStorage();
   }
 
+  function deleteBasketItem(target) {
+    const basketEl = target.closest(".product-item");
+    const basketItemsId = basketEl.dataset.id;
 
+    const basketItem = basketArr.find((item) => {
+      return +item.id === +basketItemsId;
+    });
+    if (basketItem) {
+      const index = basketArr.indexOf(basketItem);
+      basketArr.splice(index, 1);
+    }
+    renderBasket();
+    totalBasketCount();
+    totalPriceBlock();
+    saveCartToLocalStorage();
+  }
 
-
+  function totalBasketCount() {
+    const basketCountDiv = document.querySelector(".basket-count");
+    let totalCount = 0;
+    for (const item of basketArr) {
+      totalCount += item.count;
+    }
+    basketCountDiv.textContent = totalCount;
+  }
 
   function totalPriceBlock() {
     const totalPriceDiv = document.querySelector(".total-price");
@@ -135,6 +269,8 @@ const init = () => {
       totalPrice += item.price * item.count;
     }
 
+    totalPriceDiv.textContent =
+      totalPrice > 0 ? `Total Price: $${totalPrice}` : "Your basket is empty";
   }
 
   function handleSearch(e) {
@@ -210,6 +346,7 @@ const init = () => {
     const storedCartItems = localStorage.getItem("cartItems");
 
     if (storedCartItems) {
+      basketArr.push(...JSON.parse(storedCartItems));
       renderBasket();
       totalBasketCount();
       totalPriceBlock();
